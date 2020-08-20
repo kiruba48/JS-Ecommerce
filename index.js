66cf57bff0eebc443729a252a76426bca2dd9128
KIRUBAKARAN MUTHUPALANI <kiru@KIRUBAKARANsMBP.home>
2020-08-20 18:30:57 +0100

    Adding Authentication functionality

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const userRepo = require("./repositories/users.js");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const userRepo = require('./repositories/users.js');
+const { passwordCompare } = require('./repositories/users.js');
 
 const app = express();
 // Using this use method, all the route handler in our project file can us this bodyParser function.
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ['asdjsdkjfhkjd6fdds4'],
+  })
+);
 
 // Route handler
-app.get("/", (req, res) => {
+app.get('/signup', (req, res) => {
   res.send(`
     <div>
+      Your ID is: ${req.session.userId}
         <form method="POST">
             <input name = "email" placeholder = "email" />
             <input name = "password" placeholder = "password" />
             <input name = "passwordConformation" placeholder = "password conformation" />
-            <button>Sign up</button>
+            <button>Sign Up</button>
         </form>
     </div>
   `);
@@ -39,22 +47,64 @@ app.get("/", (req, res) => {
 // };
 
 // Using bodyParser Middleware npm library.
-app.post("/", async (req, res) => {
+app.post('/signup', async (req, res) => {
   const { email, password, passwordConformation } = req.body;
 
   // Checking for existing user.
   const existingUser = await userRepo.getOneBy({ email });
   if (existingUser) {
-    return res.send("User already exists");
+    return res.send('User already exists');
   }
 
   // checking for password match.
   if (password !== passwordConformation) {
-    return res.send("Password does not match");
+    return res.send('Password does not match');
   }
-  res.send("Account Created");
+
+  // Create a user in our repo.
+  const user = await userRepo.create({ email, password });
+
+  // Store the Id inside the cookie.(cookie handled using third party library)
+  req.session.userId = user.ID; //req.session created by cookie=session library.
+
+  res.send('Account Created');
+});
+
+app.get('/signout', (req, res) => {
+  req.session = null; //clear out any cookie data that you have.
+  res.send('You are signed out');
+});
+
+// signIn
+app.get('/signin', (req, res) => {
+  res.send(`
+    <div>
+        <form method="POST">
+            <input name = "email" placeholder = "email" />
+            <input name = "password" placeholder = "password" />
+            <button>Sign In</button>
+        </form>
+    </div>
+  `);
+});
+
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await userRepo.getOneBy({ email });
+  // Check for email id.
+  if (!user) {
+    return res.send('User not found');
+  }
+  // check for password.
+  const validPassword = await userRepo.passwordCompare(user.password, password);
+  if (!validPassword) {
+    return res.send('Password does not match the login ID');
+  }
+
+  req.session.userId = user.ID;
+  res.send('You are signed In');
 });
 
 app.listen(3000, () => {
-  console.log("Listening...");
+  console.log('Listening...');
 });
